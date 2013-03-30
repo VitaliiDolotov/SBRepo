4d1f186a3e09f0825d7b518f9201febf418d3439
VitaliiDolotov <vitaliy.dolotov@gmail.com>
2013-03-30 20:17:03 +0200

    Updater sln was added

---
+++ Updater/Updater/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 195);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Статус обновления:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 144);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(254, 13);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Обнаружена новая версия бота";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(254, 119);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 209);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Updater";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Updater/Updater/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace Updater
+{
+    public partial class Form1 : Form
+    {
+        string[] FilesForDeleting;
+        string[] FilesForDownload;
+        string[] FilesForDownloadNames;
+        int BotVersion = 2473;
+        int NewBotVersion;
+        string reportLog = "";
+        string labelMessage = "";
+        int percentage = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            ClosingSimpleBotProcces();
+        }
+
+        private void UpdateFileReader()
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://dl.dropbox.com/s/gixvi3853twwks3/UpdateFile.csv?dl=1");
+            StreamReader reader = new StreamReader(stream);
+            string[] FileContent = reader.ReadToEnd().Split(';');
+            NewBotVersion = Convert.ToInt32(FileContent[0]);
+            FilesForDeleting = FileContent[1].Split(',');
+            FilesForDownload = FileContent[2].Split(',');
+            FilesForDownloadNames = FileContent[3].Split(',');
+            percentage += 5;
+        }
+
+        private void FilesDeleting()
+        {
+            try
+            {
+                //Deliting files
+                for (int i = 0; i < FilesForDeleting.Length; i++)
+                {
+                    try
+                    {
+                        if (File.Exists( FilesForDeleting[i]) == true)
+                        {
+                            File.Delete(FilesForDeleting[i]);
+                            reportLog += "Файл " + FilesForDeleting[i] + " удален" + "\r\n";
+                            //textBox1.Text += "Файл " + FilesForDeleting[i] + " удален" + "\r\n";
+                        }
+                        else
+                        {
+                            reportLog += "Файл " + FilesForDeleting[i] + " не найде" + "\r\n";
+                            //textBox1.Text += "Файл " + FilesForDeleting[i] + " не найде" + "\r\n";
+                        }
+                    }
+                    catch
+                    {
+                        reportLog += "Не удалось удалить " + FilesForDeleting[i] + " файл" + "\r\n";
+                        //textBox1.Text += "Не удалось удалить " + FilesForDeleting[i] + " файл" + "\r\n";
+                    }
+                    if (percentage < 40)
+                    {
+                        percentage += 5;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Unexpected error appears", "Error during Simple Bot updating ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FilesDownloading()
+        {
+            try
+            {
+                //Downloading files
+                WebClient Client = new WebClient();
+                for (int y = 0; y < FilesForDownload.Length; y++)
+                {
+                    Client.DownloadFile(FilesForDownload[y], FilesForDownloadNames[y]);
+                    reportLog += "Файл " + FilesForDownloadNames[y] + " обновлен" + "\r\n";
+                    //textBox1.Text += "Файл " + FilesForDownloadNames[y] + " обновлен" + "\r\n";
+                    if (percentage < 89)
+                    {
+                        percentage += 10;
+                    }
+                }
+                labelMessage = "Simple Bot успешно обновлен";
+            }
+            catch
+            {
+                MessageBox.Show("Unexpected error appears", "Error during Simple Bot updating ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SimpleBotRestart ()
+        {
+            try
+            {
+                Process SimpleBotProcess = new Process();
+                SimpleBotProcess.StartInfo.FileName = "Simple Bot.exe";
+                SimpleBotProcess.Start();
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Simple Bot can't be started", "Error during Simple Bot launching ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckForUpdates()
+        {
+            percentage += 5;
+            try
+            {
+
+                if (NewBotVersion <= BotVersion)
+                {
+                    MessageBox.Show("You use latest version of Simple Bot", "Checking for updates",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Environment.Exit(0);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Simple Bot can't check latest version", "Error during updates checking ",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ClosingSimpleBotProcces()
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains("Simple Bot"))
+                {
+                    clsProcess.Kill();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            percentage = 5;
+            reportLog = "Началось обновление Simple Bot'a...\r\n";
+            button1.Enabled = false;
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+            //label1.Text = labelMessage;
+            //button2.Visible = true;
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SimpleBotRestart();
+            this.Close();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //читаем контент файла
+            UpdateFileReader();
+            //проверяем нужно ли обновлять
+            CheckForUpdates();
+            FilesDeleting();
+            FilesDownloading();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = reportLog;
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+            progressBar1.Value = percentage;
+            if (percentage == 100)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label1.Text = labelMessage;
+            button2.Visible = true;
+            percentage = 100;
+        }
+    }
+}
